0f0e0013e9c15b26324a843ce2d5c9c5282d0db4
wei.liu <wei.liu@ihandysoft.com>
2018-05-28 20:39:04 +0800

    taskFragment基本框架

--- app/src/main/java/com/example/weiliu/nfn/fragment/TaskFragment.java
+++ app/src/main/java/com/example/weiliu/nfn/fragment/TaskFragment.java
@@ -2,28 +2,51 @@ package com.example.weiliu.nfn.fragment;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.weiliu.nfn.R;
+import com.example.weiliu.nfn.adapter.TaskAdapter;
+import com.example.weiliu.nfn.bean.TaskBean;
 
-/**
- * A simple {@link Fragment} subclass.
- */
+import java.util.ArrayList;
+import java.util.List;
+
 public class TaskFragment extends Fragment {
 
-
-    public TaskFragment() {
-        // Required empty public constructor
-    }
+    private RecyclerView taskRecyclerView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_task, container, false);
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        initView(view);
+
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    private void initView(View view) {
+        taskRecyclerView = view.findViewById(R.id.rv_task);
+        TaskAdapter adapter = new TaskAdapter();
+        taskRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        taskRecyclerView.setAdapter(adapter);
+
+        getData();
+
+    }
+
+    private void getData() {
+        
+    }
 }
